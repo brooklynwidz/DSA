070b7ed4e8ce04e5c1937013d494710eddaab5eb
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-07 04:21:27 +0530

    added new methods to find missing number

--- problemsOnArray/findMissingNumber.cpp
+++ problemsOnArray/findMissingNumber.cpp
@@ -6,6 +6,7 @@ class Solution {
 
     //condition if the array is sorted then only this will work other wise this will not work
         int mysol(vector<int> &arr){
+            sort(arr.begin(), arr.end());
             int n = arr.size();
             for (int i = 0; i < n; i++){
                 if (arr[i+1] == arr[i]+1){}
@@ -28,11 +29,54 @@ class Solution {
                 if (flag == 0) return i;
             }
         }
+        
+        //hashing   TC->O(2N)
+        int better(vector<int> &arr, int N){
+            int n = arr.size();
+            int hash[N+1] = {0};
+            for (int i = 0; i < n; i++){
+                hash[arr[i]] = 1;
+            }
+
+            for (int i = 1; i < n+1; i++){
+                if (hash[i] == 0){
+                    return i;
+                }
+            }
+        }
+
+        int optimal1(vector<int> &arr, int N){
+            int sum = N*(N+1)/2; //15
+            int s1 = 0;
+            int n = arr.size();
+            for (int i = 0; i<n; i++){
+                s1+=arr[i];
+            }
+            /*
+            0+1+2+3+4+5 = 15
+            1+2+4+5 = 12
+            15-12 = 3           
+            */
+            return sum-s1;
+        }
+
+        int optimal2(vector<int> &arr, int N){
+            int XOR1 = 0; //1^2^3^4^5
+            int XOR2 = 0; //1^2^5^4
+            int n = arr.size();
+            for (int i = 0; i<n; i++){
+                XOR1 = XOR1^i+1;
+                XOR2 = XOR2^arr[i];
+            }
+            XOR1^=N;
+            return XOR1^XOR2;
+        }
 };
 
 
 int main(){
     vector<int> arr = {1,2,5,4};
     Solution s1;
-    cout << s1.brute(arr,5);
+    int N = 5;
+    cout << s1.optimal2(arr, N);
 }
