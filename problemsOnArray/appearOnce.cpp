832c0e150788188986c1faceccc7e5ac3f42c487
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-10 16:22:24 +0530

    brute method done

---
+++ problemsOnArray/appearOnce.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int brute(vector<int> &arr){
+            int cnt = 0;
+            int n = arr.size();
+            for (int i = 0; i<n; i++){
+                int num = arr[i];
+                for (int j = 0; j < n; j++){
+                    if (arr[j] == num){
+                        cnt++;
+                    }
+                } 
+                if (cnt == 1){
+                    return num;
+                }
+                else {
+                    cnt = 0;
+                }
+            }
+            return 0;
+        }
+
+        int better(vector<int> &arr){
+            int n = arr.size();
+            //here i will not use array instead i will use map so that time complexity as well as space compleaxity decreases
+        }
+
+};
+
+int main(){
+    Solution s1;
+    vector<int> arr = {1,1,2,2,3,3,4,4};
+    cout << s1.brute(arr);
+}
