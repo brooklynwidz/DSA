7aeec1972b1c23f20012e5e8e82f834fd299b793
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-11 17:57:46 +0530

    all bugs fixed

--- problemsOnArray/largestSubarray.cpp
+++ problemsOnArray/largestSubarray.cpp
@@ -7,20 +7,30 @@ class Solution {
             int n = arr.size();
             int sum = 0, length = 0, i = 0, j = 0;
             while (i < n){
-                if (j > n) {
+                if (j >= n) {
                     i++;
                     j = i;
+                    sum = 0;
                 }
-                sum = sum + arr[j];
-                if (sum >= k){
+                if (i<n && j<n) {
+                    sum = sum + arr[j];
+                }
+                if (sum == k ){
                     if ((j-i+1) > length){
                         length = j - i + 1;
                     }
+                    sum=0;
+                    i++;
+                    j=i;
+                }
+                else if (sum > k){
                     i++;
                     j=i;
                     sum = 0;
                 }
-                j++;
+                else {
+                    j++;
+                }
             }
 
             if (length == 0){
@@ -35,7 +45,8 @@ class Solution {
 
 int main(){
     Solution s1;
-    vector<int> arr = {10,5,2,7,1,9};
-    int k = 15;
+    // vector<int> arr = {10,5,2,7,1,9};
+    vector<int> arr = {1,2,1,1,1};
+    int k = 3;
     cout << s1.mySol(arr,k);
 }
