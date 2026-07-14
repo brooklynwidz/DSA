56ce55ccb288be3f78d807f213bae865838fb785
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-15 00:01:34 +0530

    optimal method for largest Subarray

--- problemsOnArray/largestSubarray.cpp
+++ problemsOnArray/largestSubarray.cpp
@@ -81,6 +81,29 @@ class Solution {
             }
             return maxLen;
         }
+
+        int optimal(vector<int> &arr, int k){
+            int left = 0, right = 0;
+            int sum = arr[0];
+            int maxLen = 0;
+            int n = arr.size();
+            while (right < n) {
+                while (left <= right && sum > k){
+                    sum-=arr[left];
+                    left++;
+
+                }
+                if (sum == k) {
+                    maxLen = max(maxLen, right - left + 1);
+                }
+                right++;
+                if (right < n){
+                    sum+=arr[right];
+                }
+            }
+
+            return maxLen;  
+        }
 };
 
 
