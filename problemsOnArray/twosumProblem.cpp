6de8d350dae5ec4a40b6721364c8142722c324ac
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-19 18:27:39 +0530

    added optimal solution

--- problemsOnArray/twosumProblem.cpp
+++ problemsOnArray/twosumProblem.cpp
@@ -29,10 +29,23 @@ class Solution{
             }
             return false;
         }
+
+        string optimal(vector<int> &arr, int target){
+            int n = arr.size();
+            int left = 0, right = n-1;
+            sort(arr.begin(),arr.end());
+            while (left<right){
+                int sum = arr[left] + arr[right];
+                if (sum == target) return "Yes";
+                else if (sum < target) left++;
+                else right--;
+            }
+            return "No";
+        }
 };
 
 int main(){
     vector<int> arr = {1,2,4,6,8,4};
     Solution s1;
-    cout << s1.brute(arr, 14);
+    cout << s1.optimal(arr, 14);
 }
