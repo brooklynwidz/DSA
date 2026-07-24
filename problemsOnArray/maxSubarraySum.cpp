76a4af86584b637d7c2457abb16ddaa9985831dc
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-25 00:18:25 +0530

    maximum subarray sum problem added

---
+++ problemsOnArray/maxSubarraySum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int brute(vector<int> &arr){
+            int maxSum = INT_MIN;
+            int n = arr.size();
+            for (int i = 0; i < n; i++){
+                for (int j = i; j < n; j++){
+                    int sum = 0;
+                    for (int k = i; k < j; k++){
+                        sum+=arr[k];
+                    }
+                    maxSum = max(sum, maxSum);
+                }
+            }
+            return maxSum;
+        }   
+
+};
+
+
+int main(){
+    vector<int> arr = {-2,2,3,-4,-5,1,2,3,6,-88};
+    Solution s1;
+    cout <<s1.brute(arr) << "\n";
+}
