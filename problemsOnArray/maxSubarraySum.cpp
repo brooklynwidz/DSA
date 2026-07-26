07d736a7afc100edab122793cb1fd1a9cf220e23
Rishabh Prajapati <rish.dash.000000@gmail.com>
2026-07-26 16:28:04 +0530

    optimal method added

--- problemsOnArray/maxSubarraySum.cpp
+++ problemsOnArray/maxSubarraySum.cpp
@@ -29,12 +29,29 @@ class Solution {
                     maxSum = max(sum,maxSum);
                 }
             }
-        }        
+        } 
+        
+        //kadanes algorithm
+        int optimal(vector<int> &arr){
+            int maxi = INT_MIN;
+            int sum = 0;
+            int n = arr.size();
+            for (int i = 0; i < n; i++){
+                sum+=arr[i];
+                if (sum < 0) {
+                    sum = 0;
+                }
+                else{
+                    maxi = max(sum,maxi);
+                }
+            }
+            return maxi;
+        }
 };
 
 
 int main(){
     vector<int> arr = {-2,2,3,-4,-5,1,2,3,6,-88};
     Solution s1;
-    cout <<s1.brute(arr) << "\n";
+    cout <<s1.optimal(arr) << "\n";
 }
