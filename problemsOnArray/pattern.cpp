7c0b06b4b67a863b077b13e3f9a5d40a27fa7991
brooklynwidz <rish.dash.000000@gmail.com>
2026-08-09 01:55:38 +0530

    optimal method added

--- problemsOnArray/pattern.cpp
+++ problemsOnArray/pattern.cpp
@@ -20,15 +20,35 @@ class Solution{
 				arr[2*i+1] = neg[i];
 			}
 		}
+		
+		vector<int> optimal(vector<int> &arr){
+			int n = arr.size();
+			int i1 = 0;
+			int i2 = 1;
+			vector<int> ans(n);
+			for (int i = 0; i < n; i++){
+				if (arr[i]>=0){
+					ans[i1] = arr[i];
+					i1+=2;
+				}
+				else {
+					ans[i2] = arr[i];
+					i2+=2;
+				}
+
+			}
+			return ans;
+		}
 	
 };
 
 int main(){
 	vector<int> arr = {3,1,-2,-5,2,-4};
 	Solution s1;
-	s1.brute(arr);
-	int n = arr.size();
+	//s1.brute(arr);
+	vector<int> ans = s1.optimal(arr);
+	int n = ans.size();
 	for (int i = 0; i<n; i++){
-		cout << arr[i] << "\n";
+		cout << ans[i] << "\n";
 	}
 }
