1fdefabc3bd2d9e17ad913a1fd3811655f2e1d0c
brooklynwidz <rish.dash.000000@gmail.com>
2026-09-03 13:16:41 +0530

    added optimal sol

--- problemsOnArray/leaders.cpp
+++ problemsOnArray/leaders.cpp
@@ -23,12 +23,26 @@ class Solution{
 
             return ans;
         }
+        //the optimal method is simple start from the back of the array make a maximum variable where you will stroe the maximum and then check for the element 
+        vector<int> optimal(vector<int> &arr){
+            int maxi = INT_MIN;
+            int n = arr.size();
+            vector<int> ans;
+            for (int i = n-1; i>=0; i--){
+                if (arr[i] > maxi){
+                    maxi = arr[i];
+                    ans.push_back(maxi);
+                }
+            } 
+
+            return ans;
+        }
 };
 
 int main(){
     Solution s1;
     vector<int> arr = {10,22,12,3,0,6};
-    vector<int> ans = s1.brute(arr);
+    vector<int> ans = s1.optimal(arr);
     sort(ans.begin(), ans.end());
     for (int i = 0; i<ans.size(); i++){
         cout << ans[i] << "\n";
